4ff683dbd98a7a55c4d421b02adced92fd88b08a
도멩가 <121483261+domangga@users.noreply.github.com>
2024-04-03 10:22:23 +0900

    Add files via upload

---
+++ 30909_손재영_0403_1/js/script.js
@@ -0,0 +1,19 @@
+$(".navi > li").mouseover(function(){
+//    $('.submenu').stop().slideDown(200);
+    $(this).children('.submenu').stop().slideDown();
+});
+$(".navi > li").mouseout(function(){
+    // $('.submenu').stop().slideUp(200);
+    $(this).children('.submenu').stop().slideUp();
+});
+
+setInterval(
+    function(){
+        $('.slidelist').delay(2000);
+        $('.slidelist').animate({marginLeft: -800});
+        $('.slidelist').delay(2000);
+        $('.slidelist').animate({marginLeft: -1600});
+        $('.slidelist').delay(2000);
+        $('.slidelist').animate({marginLeft: 0});
+    }
+);
