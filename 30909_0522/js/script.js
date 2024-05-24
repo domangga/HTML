64206119413e6371233e0c0eb17cc04f1e14ae41
도멩가 <121483261+domangga@users.noreply.github.com>
2024-05-24 12:50:36 +0900

    Add files via upload

--- 30909_0522/js/script.js
+++ 30909_0522/js/script.js
@@ -16,3 +16,13 @@ setInterval(function(){
     $('.imgslide').animate({marginLeft: 0})
     
 })
+$('.tabmenu>li>a').click(function(){
+    $(this).parent().addClass('active').siblings().removeClass('active');
+    return false;
+})
+$('.notice li:first').click(function(){
+    $('#modal').addClass('active')
+})
+$('.btn').click(function(){
+    $('#modal').removeClass('active')
+})
