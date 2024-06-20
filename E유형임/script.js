fd7ec86d98ccb91dcb6d0ddf331aeab041d103b8
도멩가 <121483261+domangga@users.noreply.github.com>
2024-06-20 16:27:01 +0900

    Add files via upload

--- E유형임/script.js
+++ E유형임/script.js
@@ -4,3 +4,14 @@ $('.navi>li').mouseover(function(){
 $('.navi>li').mouseout(function(){
     $(this).find('.sub').stop().slideUp()
 })
+setInterval(function(){
+    $('.imgslide').delay(2000)
+    $('.imgslide').animate({marginLeft : -1620 })
+    $('.imgslide').delay(2000)
+    $('.imgslide').animate({marginLeft : -3240})
+    $('.imgslide').delay(2000)
+    $('.imgslide').animate({marginLeft : 0 })
+    
+    
+
+})
